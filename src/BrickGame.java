8ac06808df79173fe8a4faa0ac2e47ff15ff4af0
SaniaRL <saniarunnfors@gmail.com>
2023-10-27 12:13:47 +0200

    Initial Commit

---
+++ src/BrickGame.java
@@ -0,0 +1,11 @@
+public class BrickGame {
+
+    public void run(){
+        System.out.println("Kör Program");
+    }
+
+    public static void main(String[] args) {
+        BrickGame brickGame = new BrickGame();
+        brickGame.run();
+    }
+}
