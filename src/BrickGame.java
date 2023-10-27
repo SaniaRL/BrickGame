4b32e82b887ccea39201fe9d9fdf00298a630c6c
SaniaRL <saniarunnfors@gmail.com>
2023-10-27 20:09:10 +0200

    Removed unused import statement to avoid warning

--- src/BrickGame.java
+++ src/BrickGame.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
